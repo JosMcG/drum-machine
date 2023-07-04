3ad67c1df0241b08336051736ba5de66da9f2f94
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 11:19:22 -0400

    adding firebase deploy files

---
+++ src/services.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR7kzUGbdkV1KDEC94qNmhAHETxlFe0JY",
+  authDomain: "drum-machine-6b278.firebaseapp.com",
+  projectId: "drum-machine-6b278",
+  storageBucket: "drum-machine-6b278.appspot.com",
+  messagingSenderId: "813215720432",
+  appId: "1:813215720432:web:d4d3cea7241474bd0db335"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
